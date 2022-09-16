eabd3b5e78ca51df84785a1461ad8d8671fa9a1f
Mateus Costa <msc.mateus@hotmail.com>
2022-09-16 16:57:44 -0500

    full total carregamento de imagem agora vai

--- src/views/Characters/components/ListItem/index.tsx
+++ src/views/Characters/components/ListItem/index.tsx
@@ -5,27 +5,41 @@ import { Character } from "../../../../interfaces";
 import { CharacterImage, CharacterName, CharacterStatus, CharacterStatusIcon, CharacterType, ImageLoadingContainer, List, ListItem as Container } from "../style";
 
 const ListItem: React.FC<{ character: Character }> = ({ character }) => {
-   const [loaded, setLoaded] = useState(true)
+   const [loaded, setLoaded] = useState(false)
    const onLoadHandle = () => {
       setLoaded(true)
    }
    useEffect(() => {
-      setLoaded(true)
-   }, [character.image])
-   return (<Container >
-      <ImageLoadingContainer style={{ display: loaded ? 'none' : 'block' }}>
-         <BounceLoader />
-      </ImageLoadingContainer>
-      <Link to={"/character-details/"+character.id} style={{ display: loaded ? 'block' : 'none' }}>
-         <CharacterImage src={character.image} onLoad={onLoadHandle} />
-      </Link>
-      <CharacterName to={"/character-details/"+character.id}>{character.name}</CharacterName>
-      <div style={{ display: "flex" }}>
-         <CharacterStatusIcon status={character.status} />
-         <CharacterStatus>{character.status}</CharacterStatus>
-      </div>
-      <CharacterType>{character.type}</CharacterType>
-   </Container>)
+      setLoaded(false)
+   }, [character.image,])
+   return loaded ?
+      (<Container >
+         <Link to={"/character-details/" + character.id}>
+            <CharacterImage src={character.image} onLoad={onLoadHandle} />
+         </Link>
+         <CharacterName to={"/character-details/" + character.id}>{character.name}</CharacterName>
+         <div style={{ display: "flex" }}>
+            <CharacterStatusIcon status={character.status} />
+            <CharacterStatus>{character.status}</CharacterStatus>
+         </div>
+         <CharacterType>{character.type}</CharacterType>
+      </Container>
+      ) : (
+         <Container >
+            <ImageLoadingContainer>
+               <BounceLoader />
+            </ImageLoadingContainer>
+            <Link to={"/character-details/" + character.id} style={{ display: 'none' }}>
+               <CharacterImage src={character.image} onLoad={onLoadHandle} />
+            </Link>
+            <CharacterName to={"/character-details/" + character.id}>{character.name}</CharacterName>
+            <div style={{ display: "flex" }}>
+               <CharacterStatusIcon status={character.status} />
+               <CharacterStatus>{character.status}</CharacterStatus>
+            </div>
+            <CharacterType>{character.type}</CharacterType>
+         </Container>
+      )
 }
 
 export default ListItem;
