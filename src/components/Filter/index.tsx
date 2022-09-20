23c15294babafca2e983a87320b2d6e7863f2fdb
Mateus Costa <msc.mateus@hotmail.com>
2022-09-20 18:56:11 -0500

    avançand no filtro

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { Dispatch, SetStateAction, ChangeEvent } from "react";
+import ReactSwitch from "react-switch";
 import { Container, TextInput } from "./style";
 
-const Filter = () => {
+type FilterProps = {
+   statusValue: string,
+   onStatusChange: Dispatch<SetStateAction<string>>,
+   textValue: string,
+   onTextChange: Dispatch<SetStateAction<string>>
+   genderValue: string,
+   onGenderChange: Dispatch<SetStateAction<string>>
+}
+
+const Filter: React.FC<FilterProps> = ({
+   onTextChange, textValue,
+   onStatusChange, statusValue,
+   onGenderChange, genderValue,
+
+}) => {
+   const onTextChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      onTextChange(e.target.value)
+   }
+   const onStatusChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+      onStatusChange(e.target.value)
+   }
+   const onGenderChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
+      onGenderChange(e.target.value)
+   }
    return (
       <Container>
          <h1>Filtro</h1>
-         <TextInput type="text" />
+         <TextInput type="text" onChange={onTextChangeHandler} />
+         <ReactSwitch
+            checked={false}
+            onChange={() => { }}
+         /> Name
+         <ReactSwitch
+            checked={false}
+            onChange={() => { }}
+         /> Type
+         <ReactSwitch
+            checked={false}
+            onChange={() => { }}
+         /> Specie
+         <label htmlFor="genderSearch">Gender:</label>
+         <select id="genderSearch" onChange={onGenderChangeHandler}>
+            <option value="">Select a gender...</option>
+            <option value="Female">Female</option>
+            <option value="Male">Male</option>
+            <option value="Genderless">Genderless</option>
+            <option value="unknown">unknown</option>
+         </select>
+         <label htmlFor="statusSearch">Status:</label>
+         <select id="statusSearch" onChange={onStatusChangeHandler}>
+            <option value="">Select a status...</option>
+            <option value="Dead">Dead</option>
+            <option value="Alive">Alive</option>
+            <option value="unknown">unknown</option>
+         </select>
       </Container>
    )
 }
--- src/views/Characters/index.tsx
+++ src/views/Characters/index.tsx
@@ -9,24 +9,37 @@ const Characters = () => {
    const [responseData, setResponseData] = useState<Info<Character[]>>({});
    const [characterList, setCharacterList] = useState<Character[]>([]);
    const [atualPage, setAtualPage] = useState(1);
+   const [nameSearch, setNameSearch] = useState("")
+   const [statusSearch, setStatusSearch] = useState("")
+   const [speciesSearch, setSpeciesSearch] = useState("")
+   const [typeSearch, setTypeSearch] = useState("")
+   const [genderSearch, setGenderSearch] = useState("")
    useEffect(() => {
       axios.get('https://rickandmortyapi.com/api/character', {
          params: {
-            page: atualPage
+            page: atualPage,
+            name: nameSearch,
+            status: statusSearch,
+            species: speciesSearch,
+            type: typeSearch,
+            gender: genderSearch
          }
       })
          .then((response) => {
             setCharacterList(response.data.results)
             setResponseData(response.data)
          })
-   }, [atualPage,])
+   }, [atualPage, nameSearch, statusSearch, speciesSearch, typeSearch, genderSearch,])
    return (
       <div>
          <h1>
             Characters
          </h1>
-         <Filter/>
-         {characterList.length > 0 ? <CharacterList list={characterList} />: null}
+         <Filter
+            textValue={nameSearch} onTextChange={setNameSearch}
+            genderValue={genderSearch} onGenderChange={setGenderSearch}
+            onStatusChange={setStatusSearch} statusValue={statusSearch} />
+         {characterList.length > 0 ? <CharacterList list={characterList} /> : null}
          <Paginator atualPage={atualPage}
             count={responseData.info ? responseData.info.count : undefined}
             pages={responseData.info ? responseData.info.pages : undefined}
